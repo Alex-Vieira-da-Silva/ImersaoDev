a9fefda8db4dd8ed792728f6ef245fa98123a0b1
Alex-Vieira-da-Silva <alexvieira.360@hotmail.com>
2024-09-06 19:04:56 -0300

    commit projeto finalizado

---
+++ js/app.js
@@ -0,0 +1,57 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Se o campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p class='font-color-meta'>Nada foi encontrado. Voce precisa digita o nome de um filme ou série.</p>"
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let genero = "";
+    let tags = "";
+
+
+    // Itera sobre cada elemento (dado) no array de dados
+    for (let dado of dados) {
+
+        titulo = dado.titulo.toLowerCase();
+        genero = dado.genero.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        // Retorna apenas itens que estão na lista
+        if (titulo.includes(campoPesquisa) || genero.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Constrói o HTML para cada item do resultado da pesquisa
+            resultados += `
+            <div class="item-resultado">
+                <h2> <a href="#" target="_blank">${dado.titulo}</a></h2>
+                <p class="font-color-meta">Descrição: ${dado.descricao}</p>
+                <p class ="font-color-meta">Gênero: ${dado.genero}</p>
+                <p class ="font-color-meta">Ano: ${dado.ano}</p>
+                <p class="font-color-meta">Classificação: ${dado.classificacao}</p>
+                <p class="font-color-meta">Elenco: ${dado.elenco}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p class='font-color-meta'>Nada foi encontrado.</p>";
+    }
+
+    // Atribui a string com os resultados formatados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+
+}
+
+
---
+++ js/dados.js
@@ -0,0 +1,88 @@
+let dados = [
+    {
+        titulo: "SWAT",
+        descricao: "A Série S.W.A.T. na Netflix: Uma Equipe de Elite A série S.W.A.T. acompanha uma unidade de elite do Departamento de Polícia de Los Angeles. A equipe é composta por policiais altamente treinados que lidam com as situações mais perigosas da cidade.",
+        link: "https://pt.wikipedia.org/wiki/S.W.A.T._(série_de_televisão)",
+        genero: "Ação, Policial",
+        ano: 2017,
+        classificacao: "16 anos",
+        elenco: ["Alex Russell", "Shemar Moore", "Jay Harrington"],
+        tags: "Ação ação acao Luta luta Policial policial"
+    },
+    {
+        titulo: "Stranger Things",
+        descricao: "Uma série de ficção científica ambientada nos anos 80, que acompanha um grupo de amigos em busca de um amigo desaparecido, enquanto enfrentam forças sobrenaturais.",
+        link: "https://pt.wikipedia.org/wiki/Stranger_Things",
+        genero: "Ficção Científica, Terror",
+        ano: 2016,
+        classificacao: "16 anos",
+        elenco: ["Millie Bobby Brown", "Finn Wolfhard", "David Harbour"],
+        tags: "Ficção ficção terror terror antigo"
+    },
+    {
+        titulo: "La Casa de Papel",
+        descricao: "Um grupo de assaltantes habilidosos planeja um dos maiores roubos da história, invadindo a Casa da Moeda da Espanha.",
+        link: "https://pt.wikipedia.org/wiki/La_casa_de_papel",
+        genero: "Drama, Suspense",
+        ano: 2017,
+        classificacao: "16 anos",
+        elenco: ["Úrsula Corberó", "Álvaro Morte", "Itziar Ituño"],
+        tags: "drama Drama historia, história, Suspense suspense"
+    },
+    {
+        titulo: "Black Mirror",
+        descricao: "Uma antologia que explora as consequências da tecnologia em nossa sociedade, apresentando histórias independentes com temas como realidade virtual, redes sociais e inteligência artificial.",
+        link: "https://pt.wikipedia.org/wiki/Black_Mirror",
+        genero: "Ficção Científica, Antologia",
+        ano: 2011,
+        classificacao: "16 anos",
+        elenco: "Varia a cada episódio",
+        tags: "Antologia antologia sociedade"
+    },
+    {
+        titulo: "The Witcher",
+        descricao: "Baseada nos livros de fantasia de Andrzej Sapkowski, a série acompanha Geralt de Rivia, um caçador de monstros solitário, que se vê envolvido em uma saga épica.",
+        link: "https://pt.wikipedia.org/wiki/The_Witcher_(série_de_televisão)",
+        genero: "Fantasia, Aventura",
+        ano: 2019,
+        classificacao: "16 anos",
+        elenco: ["Henry Cavill", "Anya Chalotra", "Freya Allan"],
+        tags: "Fantasia fantasia Aventura aventura monstros"
+    },
+    {
+        titulo: "Bridgerton",
+        descricao: "Um drama de época que se passa na alta sociedade londrina do século XIX, explorando temas como amor, família e escândalos.",
+        link: "https://pt.wikipedia.org/wiki/Bridgerton",
+        genero: "Drama, Romance, Época",
+        ano: 2020,
+        classificacao: "16 anos",
+        elenco: ["Phoebe Dynevor", "Regé-Jean Page", "Golda Rosheuvel"],
+        tags: "Drama drama Romance romance antigo famíla"
+    },
+    {
+        titulo: "Ozark",
+        descricao: "Um drama criminal que acompanha um consultor financeiro que se muda para os arredores do lago de Ozark e se envolve com um cartel mexicano.",
+        link: "https://pt.wikipedia.org/wiki/Ozark_(série_de_televisão)",
+        genero: "Drama, Crime",
+        ano: 2017,
+        classificacao: "16 anos",
+        elenco: ["Jason Bateman", "Laura Linney", "Julia Garner"],
+        tags: "Crime crime Drama drama financeiro Financeiro"
+    },
+    {
+        titulo: "Vikings",
+        descricao: "Uma série histórica que retrata as aventuras de Ragnar Lothbrok, um dos mais famosos líderes vikings. Essa série nos transporta para o mundo brutal e misterioso de Ragnar Lothbrok, um guerreiro viking e fazendeiro que anseia explorar – e saquear – as distantes costas do oceano. Vikings é uma produção que mistura aventura, drama e história, e ela tem conquistado fãs em todo o mundo.",
+        link: "https://pt.wikipedia.org/wiki/Vikings",
+        genero: "Aventura, Drama, Histórico",
+        ano: 2013,
+        classificacao: "16 anos",
+        elenco: ["Travis Fimmel", "Katheryn Winnick", "Clive Standen"],
+        tags: "Aventura aventura drama Drama histórico"
+    }
+
+];
+
+
+
+
+
